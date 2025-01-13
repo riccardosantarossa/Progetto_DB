7b4c3d5122fa39ac8662d75af97af5ff0e1101f8
Riccardo Santarossa <brudo1968@libero.it>
2025-01-13 16:29:08 +0100

    Ordine nel posto giusto

--- DDL.sql
+++ DDL.sql
@@ -1,34 +1,36 @@
 CREATE TABLE Reparto (
 Nome VARCHAR(256),
 Numero INTEGER PRIMARY KEY,
-Caporeparto VARCHAR(16)
-);
+Caporeparto VARCHAR(16));
+
 CREATE TABLE Dipendente (
 CF VARCHAR(16) PRIMARY KEY,
 Nome VARCHAR(256),
 Stipendio FLOAT,
 Indirizzo VARCHAR(256),
-NumeroReparto INTEGER
-);
+NumeroReparto INTEGER);
+
 ALTER TABLE Reparto
 ADD CONSTRAINT FK_Caporeparto FOREIGN KEY (Caporeparto) REFERENCES
 Dipendente(CF);
+
 ALTER TABLE Dipendente
 ADD CONSTRAINT FK_NumeroReparto FOREIGN KEY (NumeroReparto) REFERENCES
 Reparto(Numero);
+
 CREATE TABLE Prodotto(
 Codice INTEGER,
 PrezzoVendita FLOAT,
 Nome VARCHAR(256),
 NumeroReparto INTEGER,
 PRIMARY KEY(Codice),
-FOREIGN KEY (NumeroReparto) REFERENCES REPARTO(Numero)
-);
+FOREIGN KEY (NumeroReparto) REFERENCES REPARTO(Numero));
+
 CREATE TABLE Fornitore(
 Nome VARCHAR(256),
 Indirizzo VARCHAR(256),
-PRIMARY key (Nome)
-);
+PRIMARY key (Nome));
+
 CREATE TABLE NumeroTelefono(
 NumeroTel VARCHAR(12),
 Azienda VARCHAR(256),
@@ -58,16 +60,16 @@ PRIMARY key(NumeroOrdine,NumReparto)
 CREATE TABLE Cliente (
 Nome VARCHAR(256),
 Indirizzo VARCHAR(256),
-Saldo FLOAT,
-PRIMARY KEY (Nome)
-);
+PRIMARY KEY (Nome));
+
 CREATE TABLE OrdineCliente (
 NumeroOrdine INTEGER,
 Cliente VARCHAR(256),
 DataOrdine DATE,
+Saldo FLOAT,
 FOREIGN KEY (Cliente) REFERENCES Cliente(Nome),
-PRIMARY KEY (NumeroOrdine, Cliente)
-);
+PRIMARY KEY (NumeroOrdine, Cliente));
+
 CREATE TABLE CheCosa (
 NumeroOrdine INTEGER,
 Cliente VARCHAR(256),
