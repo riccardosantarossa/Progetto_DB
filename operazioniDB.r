301f639946e4e8e4b051810a9f0ed23649824102
riccardosantarossa <rikysanta03@gmail.com>
2025-02-04 18:31:02 +0100

    Popolato fornisce

--- operazioniDB.r
+++ operazioniDB.r
@@ -1,5 +1,5 @@
 
-setwd("C:/Users/brudo/Desktop/Progetto_DB")
+setwd("C:/Users/Utente/Desktop/Progetto_DB")
 
 # Connessione con RPostgres
 library(RPostgres)
@@ -45,8 +45,7 @@ insertTransactionsManagerReparto <- function(file)
     vl <- as.list(c(cf,nrep))
 
     df <- dbGetQuery(connect, "SELECT 1 FROM reparto r, dipendente d WHERE d.cf = r.caporeparto AND d.cf = $1 AND d.numeroreparto = r.numero AND r.numero = $2;", vl)
-    print(df)
-    
+
     if(is.null(df[1,1]))
     {
       dbRollback(connect)
@@ -58,6 +57,17 @@ insertTransactionsManagerReparto <- function(file)
   }
   close(fopen)
 }
+
+#Funzione che genera gli ordini per ogni reparto
+ordiniPerReparto <- function(numReparto)
+{
+  datePossibili <- read.csv('date.csv')
+  
+  prodottiReparto <- dbGetQuery(connect, "SELECT codice FROM prodotto WHERE numeroreparto = $1", numReparto)
+  ordineReparto <- data.frame(numeroordine = seq(from = numReparto, to = 100*num), numreparto = rep(numReparto:numReparto, each = 100),
+                              dataordine = sample(datePossibili, 100, replace = FALSE, ))
+}
+
 
 #Popolamento dei clienti
 #inserimento("cliente.sql")
@@ -77,17 +87,22 @@ numeriTelefono <- cbind(numerotel, aziendeFin)
 #dbWriteTable(connect, name = "numerotelefono", value = numeriTelefono, append = TRUE, row.names = FALSE)
 
 #Popolamento reparto-manager con transazioni
-insertTransactionsManagerReparto("transactions.txt")
+#insertTransactionsManagerReparto("transactions.txt")
 
 #Popolamento dipendenti
-inserimento("dipendente.sql")
+#inserimento("dipendente.sql")
 
 #Popolamento prodotti
-inserimento("prodotto.sql")
+#inserimento("prodotto.sql")
 
+#Popolamento delle forniture dei prodotti
+fornitori <- dbGetQuery(connect, "SELECT nome FROM fornitore")
+prodotti <- dbGetQuery(connect, "SELECT codice FROM prodotto")
+forniture <- data.frame(fornitore = sample(fornitori$nome, 50, replace = TRUE), prodotto = sample(prodotti$codice, 300, replace = FALSE), 
+                        prezzofornitura = round(runif(n = 300, min = 1, max = 100), 2))
+#dbWriteTable(connect, name = "fornisce", value = forniture, append = TRUE, row.names = FALSE)
 
-
-
-
-
+#Popolamento degli ordini dei reparti
+dati <- dbGetQuery(connect, "select prodotto, numeroreparto, fornitore from fornisce f, prodotto p where f.prodotto = p.codice")
+ordiniReparto <- data.frame(numeroordine = 1:1000, numreparto = )
 
